0449251aade4d447a2be36bb5e2aba05d75026e8
cyrillouis2526 <cyril.louis@viacesi.fr>
2025-10-22 14:09:43 +0200

    powershell_UO

---
+++ adds.ps1
@@ -0,0 +1,4 @@
+New-ADOrganizationalUnit -name:"STAGIARE" -path:"DC=Cesi,DC=fr" -ProtectedFromAccidentalDeletion:$true -Server:"WIN-RM7C6M5KT90.Cesi.FR"
+New-ADOrganizationalUnit -name:"ADMINISTRATION" -path:"DC=Cesi,DC=fr" -ProtectedFromAccidentalDeletion:$true -Server:"WIN-RM7C6M5KT90.Cesi.FR"
+New-ADOrganizationalUnit -name:"CHAUFFEUR" -path:"DC=Cesi,DC=fr" -ProtectedFromAccidentalDeletion:$true -Server:"WIN-RM7C6M5KT90.Cesi.FR"
+New-ADOrganizationalUnit -name:"COMPTABLE" -path:"DC=Cesi,DC=fr" -ProtectedFromAccidentalDeletion:$true -Server:"WIN-RM7C6M5KT90.Cesi.FR"
